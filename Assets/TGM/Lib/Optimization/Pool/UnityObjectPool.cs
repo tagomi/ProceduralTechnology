1cce74b59ebd825db5a7be853a9868b2a3e05115
ただのごみ <tagomin1124@gmail.com>
2018-04-02 00:12:06 +0900

    [clean]新しいファイル用のコメントテンプレートを適用

--- Assets/TGM/Lib/Optimization/Pool/UnityObjectPool.cs
+++ Assets/TGM/Lib/Optimization/Pool/UnityObjectPool.cs
@@ -1,16 +1,17 @@
 ﻿// ***********************************************************************
 // Assembly         : Assembly-CSharp
 // Author           : ただのごみ
-// Created          : 03-31-2018
+// Created          : 04-01-2018
 //
 // Last Modified By : ただのごみ
 // Last Modified On : 04-01-2018
 // ***********************************************************************
-// <copyright file="GameObjectPool.cs" company="">
-//     Copyright (c) ただのごみ. Released under the MIT license.
+// <copyright file="UnityObjectPool.cs" company="">
+//     Copyright (c) ただのごみ. Please read LICENSE file. If it is nothing, all rights reserved.
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
+
 using System;
 using System.Linq;
 using UniRx;
