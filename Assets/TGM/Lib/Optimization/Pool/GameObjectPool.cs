797ccecb6632af541fc76812d3cc3568939417f9
ただのごみ <tagomin1124@gmail.com>
2018-04-17 23:36:16 +0900

    [update]回収条件をGameObjectのInactiveにできるコンストラクタを追加

--- Assets/TGM/Lib/Optimization/Pool/GameObjectPool.cs
+++ Assets/TGM/Lib/Optimization/Pool/GameObjectPool.cs
@@ -24,6 +24,11 @@ namespace TGM.Lib.Optimization.Pool
 	public class GameObjectPool : UnityObjectPool<GameObject>
 	{
 		/// <summary>
+		/// GameObjectがInactiveかを判定するデリゲート
+		/// </summary>
+		private static readonly Predicate<GameObject> IsInactiveDelegate = (gameObject => gameObject.activeSelf);
+
+		/// <summary>
 		/// コンストラクタ <see cref="GameObjectPool" /> class.
 		/// </summary>
 		/// <param name="capacity">プール可能数</param>
@@ -48,6 +53,38 @@ namespace TGM.Lib.Optimization.Pool
 		/// <param name="settlingAfterCollectingAction">オブジェクトの改修後に行う処理</param>
 		/// <param name="settlingAfterRemovingAction">オブジェクトプールから取り除いた後に行う処理</param>
 		public GameObjectPool(int capacity, int initCount, GameObject original, Predicate<GameObject> collectingPredicate, Action<GameObject> preparingToGetAction = null, Action<GameObject> settlingAfterCollectingAction = null, Action<GameObject> settlingAfterRemovingAction = null) : base(capacity, initCount, GameObjectPool.CreateInstantiateDelegate(original), collectingPredicate, preparingToGetAction, settlingAfterCollectingAction, settlingAfterRemovingAction)
+		{
+		}
+
+		/// <summary>
+		/// コンストラクタ <see cref="GameObjectPool" /> class.
+		/// </summary>
+		/// <remarks>
+		/// プールへの回収条件はプールされているGameObjectがInactiveであること
+		/// </remarks>
+		/// <param name="capacity">プール可能数</param>
+		/// <param name="initCount">最初にプールするオブジェクト数</param>
+		/// <param name="createDelegate">プールオブジェクトを作る処理</param>
+		/// <param name="preparingToGetAction">プールされているオブジェクトを取得する前に行う処理</param>
+		/// <param name="settlingAfterCollectingAction">オブジェクトの改修後に行う処理</param>
+		/// <param name="settlingAfterRemovingAction">オブジェクトプールから取り除いた後に行う処理</param>
+		public GameObjectPool(int capacity, int initCount, ObjectPool<GameObject>.CreateDelegate createDelegate, Action<GameObject> preparingToGetAction = null, Action<GameObject> settlingAfterCollectingAction = null, Action<GameObject> settlingAfterRemovingAction = null) : base(capacity, initCount, createDelegate, GameObjectPool.IsInactiveDelegate, preparingToGetAction, settlingAfterCollectingAction, settlingAfterRemovingAction)
+		{
+		}
+
+		/// <summary>
+		/// コンストラクタ <see cref="GameObjectPool" /> class.
+		/// </summary>
+		/// <remarks>
+		/// プールへの回収条件はプールされているGameObjectがInactiveであること
+		/// </remarks>
+		/// <param name="capacity">プール可能数</param>
+		/// <param name="initCount">最初にプールするオブジェクト数</param>
+		/// <param name="original">The original.</param>
+		/// <param name="preparingToGetAction">プールされているオブジェクトを取得する前に行う処理</param>
+		/// <param name="settlingAfterCollectingAction">オブジェクトの改修後に行う処理</param>
+		/// <param name="settlingAfterRemovingAction">オブジェクトプールから取り除いた後に行う処理</param>
+		public GameObjectPool(int capacity, int initCount, GameObject original, Action<GameObject> preparingToGetAction = null, Action<GameObject> settlingAfterCollectingAction = null, Action<GameObject> settlingAfterRemovingAction = null) : base(capacity, initCount, GameObjectPool.CreateInstantiateDelegate(original), GameObjectPool.IsInactiveDelegate, preparingToGetAction, settlingAfterCollectingAction, settlingAfterRemovingAction)
 		{
 		}
 
