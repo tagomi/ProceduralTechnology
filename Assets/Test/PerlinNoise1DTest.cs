3ad5de3cf0d87826ad671c0943eb11fb0c583071
ただのごみ <tagomin1124@gmail.com>
2018-04-29 00:43:14 +0900

    [add]1Dパーリンノイズのテスト用

---
+++ Assets/Test/PerlinNoise1DTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TGM.Lib.Utility;
+
+public class PerlinNoise1DTest : MonoBehaviour
+{
+	public int seed;
+	public float amplitude;
+	public float accuracy;
+	public int maxStep;
+	public float width;
+
+	// Use this for initialization
+	private void Start()
+	{
+		var perlin = new TGM.Lib.Math.PerlinNoise(this.seed, this.amplitude);
+		var line = this.GetOrAddComponent<LineRenderer>();
+		line.positionCount = this.maxStep;
+
+		for (int i = 0; i < this.maxStep; i++)
+		{
+			float x = this.accuracy * (float)i;
+			float value = perlin.PerlinNoise1D(x);
+
+			line.SetPosition(i, new Vector3(x * this.width, 0f, value));
+		}
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	}
+}
