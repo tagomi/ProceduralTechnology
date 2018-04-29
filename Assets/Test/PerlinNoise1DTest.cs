41580be1f5629acf3ae2aab6dae56c413ddaa8e4
ただのごみ <tagomin1124@gmail.com>
2018-04-29 15:00:04 +0900

    [clean]テスト用のクラスにも適当なコメントをつけておく

--- Assets/Test/PerlinNoise1DTest.cs
+++ Assets/Test/PerlinNoise1DTest.cs
@@ -1,17 +1,51 @@
-﻿using System.Collections;
+﻿// ***********************************************************************
+// Assembly         : Assembly-CSharp
+// Author           : ただのごみ
+// Created          : 04-29-2018
+//
+// Last Modified By : ただのごみ
+// Last Modified On : 04-29-2018
+// ***********************************************************************
+// <copyright file="PerlinNoise1DTest.cs" company="">
+//     Copyright (c) ただのごみ. Please read LICENSE file. If it is nothing, all rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TGM.Lib.Utility;
 
+/// <summary>
+/// Class PerlinNoise1DTest.
+/// </summary>
 public class PerlinNoise1DTest : MonoBehaviour
 {
+	/// <summary>
+	/// The seed
+	/// </summary>
 	public int seed;
+	/// <summary>
+	/// The amplitude
+	/// </summary>
 	public float amplitude;
+	/// <summary>
+	/// The accuracy
+	/// </summary>
 	public float accuracy;
+	/// <summary>
+	/// The maximum step
+	/// </summary>
 	public int maxStep;
+	/// <summary>
+	/// The width
+	/// </summary>
 	public float width;
 
 	// Use this for initialization
+	/// <summary>
+	/// Starts this instance.
+	/// </summary>
 	private void Start()
 	{
 		var perlin = new TGM.Lib.Math.PerlinNoise1D(this.seed, this.amplitude);
@@ -28,6 +62,9 @@ public class PerlinNoise1DTest : MonoBehaviour
 	}
 
 	// Update is called once per frame
+	/// <summary>
+	/// Updates this instance.
+	/// </summary>
 	private void Update()
 	{
 	}
