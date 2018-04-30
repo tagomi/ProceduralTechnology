b1757b089d85290f1a6fe854c7bac5bb043a71f9
ただのごみ <tagomin1124@gmail.com>
2018-04-30 19:35:07 +0900

    [change]この関数は作って内部で持っておくだけのものに

--- Assets/Scripts/TGM/Procedural/Map/MapBehaviour.cs
+++ Assets/Scripts/TGM/Procedural/Map/MapBehaviour.cs
@@ -38,8 +38,7 @@ namespace TGM.Procedural.Map
 		/// チャンク生成
 		/// </summary>
 		/// <param name="chunkPos">チャンク座標</param>
-		/// <returns>生成されたチャンク</returns>
-		private Chunk CreateChunk(IntVector3 chunkPos)
+		public void CreateChunk(IntVector3 chunkPos)
 		{
 			// 1チャンク分の属性を決める
 			var attributes = this.mapGenerator.CreateChunkBlockAttributes(chunkPos);
@@ -53,7 +52,7 @@ namespace TGM.Procedural.Map
 			if ((attributes.GetLength(0) != blocks.GetLength(0)) || (attributes.GetLength(1) != blocks.GetLength(1)) || (attributes.GetLength(2) != blocks.GetLength(2)))
 			{
 				Debug.LogError("生成されたブロック属性とブロックそのもの配列のサイズが合致しません");
-				return null;
+				return;
 			}
 
 			// ブロックを作って配置する
@@ -77,7 +76,8 @@ namespace TGM.Procedural.Map
 			// 作り直す
 			chunk.Renew(blocks, chunkPos);
 
-			return chunk;
+			// 管理用の辞書に入れておく
+			this.chunkDic[chunkPos] = chunk;
 		}
 	}
 }
