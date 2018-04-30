f12f3678340ebfdc88afc4c2a02a04e974150ec9
ただのごみ <tagomin1124@gmail.com>
2018-04-30 17:06:26 +0900

    [add]ブロックそのものを表すBehaviour

---
+++ Assets/Scripts/TGM/Procedural/Map/BlockBehaviour.cs
@@ -0,0 +1,44 @@
+﻿// ***********************************************************************
+// Assembly         : Assembly-CSharp
+// Author           : ただのごみ
+// Created          : 04-30-2018
+//
+// Last Modified By : ただのごみ
+// Last Modified On : 04-30-2018
+// ***********************************************************************
+// <copyright file="BlockBehaviour.cs" company="">
+//     Copyright (c) ただのごみ. Please read LICENSE file. If it is nothing, all rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using UnityEngine;
+
+namespace TGM.Procedural.Map
+{
+	/// <summary>
+	/// ブロック
+	/// </summary>
+	/// <seealso cref="UnityEngine.MonoBehaviour" />
+	public class BlockBehaviour : MonoBehaviour
+	{
+		/// <summary>
+		/// ブロックの属性
+		/// </summary>
+		protected BlockAttribute attribute;
+
+		/// <summary>
+		/// ブロックの状態
+		/// </summary>
+		protected BlockStatus status = new BlockStatus();
+
+		/// <summary>
+		/// 作り直す
+		/// </summary>
+		/// <param name="attribute">属性</param>
+		public void Renew(BlockAttribute attribute)
+		{
+			this.attribute = attribute;
+		}
+	}
+}
